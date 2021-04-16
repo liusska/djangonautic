2b0f18926d3cdfa47b8748516b0d09ab48ea49c8
unknown <lyudmila_ds@abv.bg>
2021-04-16 15:15:29 +0300

    Required login to create in app

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from . import views
-from django.urls import re_path
-from django.views.decorators.http import require_GET
-from django.urls import include, re_path
+
 
 app_name = 'articles'
 
 urlpatterns = [
     url(r'^$', views.article_list, name='list'),
-    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
+    url(r'^create/$', views.article_create, name='create'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
-from django.views.decorators.http import require_GET
+from django.contrib.auth.decorators import login_required
 
 
 def article_list(request):
@@ -10,6 +10,10 @@ def article_list(request):
 
 
 def article_detail(request, slug):
-    # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+    return render(request, 'articles/article_create.html')
