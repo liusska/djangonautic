a5b5f3ba54b0975a80f5129cff56804552c9b45c
unknown <lyudmila_ds@abv.bg>
2021-04-16 12:04:16 +0300

    Creating blog using tutorial

---
+++ djangonautic/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+    # add in author later
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50] + '...'
+
+
+# python manage.py makemigrations
+# python manage.py migrate
---
+++ djangonautic/articles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Article
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+
+
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
